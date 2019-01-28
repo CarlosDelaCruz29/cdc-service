6935f9ddc0893cd3ea107ecec51eaf09a7a9cc02
MSI\Carlos Dela Cruz <carlosdelacruz29@yahoo.com.au>
2019-01-28 17:58:55 +1100

    Testing and Comments

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 const bodyParser = require('body-parser')
 const express = require('express');
 const app = express();
-const ShowHelper = require('./ShowHelper');
+const ShowHelper = require('./source/ShowHelper');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+// Handle the POST request sent to this service
 app.post("/", (req, res, next) => {
+    
+    // initialize ShowHelper
     let showHelper = new ShowHelper();
+
+    // get a filtered and formatted array of the shows
     let filteredAndFormattedShows = showHelper.getFilteredAndFormattedShows(req.body);
-    res.status(200).send(filteredAndFormattedShows);
+
+    // return the array if it exists (no errors in JSON)
+    if (filteredAndFormattedShows) {
+        res.status(200).send({
+            response: filteredAndFormattedShows
+        });
+    } else {
+        next();
+    }
+});
+
+// catches the errors that go past POST
+app.use((req, res, next) => {
+    const error = new Error();
+
+    // set the status to 400 (Bad Requests)
+    error.status = 400;
+    next(error);
+});
+
+// handles the errors and returns error data
+app.use((error, req, res, next) => {
+    // error message to be sent
+    let errorMessage = "Could not decode request: JSON parsing failed";
+    res.status(error.status);
+    res.json({
+        error: errorMessage
+    });
 });
 
 module.exports = app;
---
+++ source/ShowHelper.js
@@ -0,0 +1,71 @@
+/**
+ * Helper class used to perform filtering and formatting functions for the web service
+ */
+class ShowHelper {
+
+    /**
+     * Empty Constructor
+     */
+    ShowHelper() {}
+
+    /**
+     * Returns filtered and formatted show data
+     * @param {Object} showData the data sent to the service
+     * @returns {Object} will either return an array of the formatted shows or null
+     */
+    getFilteredAndFormattedShows(showData) {
+        // if there is no data sent to the service, return null
+        if(showData != null) {
+            // get the payload from the sent data
+            let payload = showData.payload;
+            
+            // filter the shows
+            let filteredShows = this.getFilteredShows(payload);
+
+            // format what data to keep / show, if there are no shows in the array, return null
+            let formattedShows = (filteredShows) ? this.formatShows(filteredShows) : null;
+            
+            return formattedShows;
+        }
+
+        return null;
+    }
+
+    /**
+     * Filter the provided payload
+     * @param {Object} payload the list of shows inside of the sent data (payload)
+     * @returns {Object} returns the filtered show data, if the payload does not exist, return null
+     */
+    getFilteredShows(payload) {
+        // return null if the payload does not exist
+        if(payload != null) {
+            return payload.filter((show) => {
+                // return shows with:
+                // - DRM enabled (drm: true) 
+                // - at least one episode (episodeCount > 0)
+                // - slug, show image, and title exist
+                return show.drm == true && show.episodeCount > 0 
+                    && show.slug != null && show.image != null
+                    && show.image.showImage != null && show.title != null;
+            });
+        }
+        return null;
+    }
+
+    /**
+     * Formats the showdata to only include the image, slug and title of each shows
+     * @param {Object} shows list of show data
+     * @returns {Object} the formatted list of show data
+     */
+    formatShows(shows) {
+        return shows.map((show) => {
+            return {
+                "image": show.image.showImage,
+                "slug": show.slug,
+                "title": show.title
+            }
+        });
+    }
+}
+
+module.exports = ShowHelper;
